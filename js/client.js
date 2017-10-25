81dcca0bd0f335a77b262c8d1c40ecc5726d76ac
Julian Curtis-Zilius <juliancurtiszilius@gmail.com>
2017-10-25 17:47:48 -0400

    test

--- js/client.js
+++ js/client.js
@@ -1,12 +1,6 @@
-var peer = new Peer({key: 'uhlsr8l6r5c7syvi'});
+var peer = new Peer({key: 'uhlsr8l6r5c7syvi', debug: true});
 var video = document.querySelector('video');
 
 var canvas = document.querySelector('canvas');
 var stream = canvas.captureStream(1);
 
-// Call a peer, providing our mediaStream
-var call = peer.call('qubucljenfmyrpb9', stream);
-
-call.on('stream', function(stream) {
-	video.srcObject = stream;
-});
