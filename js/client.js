8980939aeea3735e75deebfcea87be4a953db5c3
Julian Curtis-Zilius <juliancurtiszilius@gmail.com>
2017-10-25 17:49:45 -0400

    testing

--- js/client.js
+++ js/client.js
@@ -5,9 +5,3 @@ var video = document.querySelector('video');
 var canvas = document.querySelector('canvas');
 var stream = canvas.captureStream(1);
 
-// Call a peer, providing our mediaStream
-var call = peer.call('qubucljenfmyrpb9', stream);
-
-call.on('stream', function(stream) {
-	video.srcObject = stream;
-});
