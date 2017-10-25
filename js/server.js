afd2f41e08609ca05c8dae83786914746a0fef32
Julian Curtis-Zilius <juliancurtiszilius@gmail.com>
2017-10-25 16:29:32 -0400

    testing

--- js/server.js
+++ js/server.js
@@ -1,13 +1,29 @@
 // Variables
 var extensionId = "oflocnpllkmhafmbbhcfkibjmadimlof";
 
-$(document).ready(function() {
+function startStreaming() {
+	// Initiate the streaming flow
+	chrome.runtime.sendMessage(extensionId, {startStreaming: true}, function(response) {
+		if (response.false) {
+			console.log('failed to start streaming')
+		} else {
+
+		}
+	});
+}
+
+function checkInstallStatus() {
 	// First, we check if the extension is installed.
 	chrome.runtime.sendMessage(extensionId, {isInstalled: true}, function(response) {
 	    if (response) {
-	      $('#installed').removeClass('invisible');
+	      $('#installed').removeClass('d-none');
+	      startStreaming();
 	    } else {
-	      $('#not-installed').removeClass('invisible');
+	      $('#not-installed').removeClass('d-none');
 	    }
 	});
+}
+
+$(document).ready(function() {
+	checkInstallStatus();
 });
