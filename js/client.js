6d1a0e9c6a6f019cd16571d655e24f1e8306d2e2
Julian Curtis-Zilius <juliancurtiszilius@gmail.com>
2017-10-25 18:36:29 -0400

    test

--- js/client.js
+++ js/client.js
@@ -2,9 +2,13 @@ var peer = new Peer({key: 'y180ev3ts2zw7b9', debug: true});
 
 var video = document.querySelector('video');
 
-// Call a peer, providing our mediaStream
-var call = peer.call('qwr04du17l8ilik9', document.createElement('canvas').captureStream(0));
+$(document).ready(function () {
+	$('#join').click(function() {
+		var call = peer.call($('#stream-id').val(), document.createElement('canvas').captureStream(0));
 
-call.on('stream', function(stream) {
-	video.srcObject = stream;
+		call.on('stream', function(stream) {
+			video.srcObject = stream;
+		});
+	});
 });
+
