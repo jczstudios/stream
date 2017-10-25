d2226ade2da174a7440138b0d240a3aef0459f6b
Julian Curtis-Zilius <juliancurtiszilius@gmail.com>
2017-10-25 17:48:49 -0400

    changed key

--- js/client.js
+++ js/client.js
@@ -1,6 +1,13 @@
-var peer = new Peer({key: 'uhlsr8l6r5c7syvi', debug: true});
+var peer = new Peer({key: 'y180ev3ts2zw7b9', debug: true});
+
 var video = document.querySelector('video');
 
 var canvas = document.querySelector('canvas');
 var stream = canvas.captureStream(1);
 
+// Call a peer, providing our mediaStream
+var call = peer.call('qubucljenfmyrpb9', stream);
+
+call.on('stream', function(stream) {
+	video.srcObject = stream;
+});
